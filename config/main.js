d484a57dc318dd9f4cf73d359c020ec559ffc875
Olatayo <useful.kyo@gmail.com>
2019-04-04 22:18:55 +0100

    main config edited

--- config/main.js
+++ config/main.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secret': '8788',
-    'database': 'mongodb://127.0.0.1:27017/Appraisal'
-    //'database': 'mongodb://db/staffAppraisal'
+    //'database': 'mongodb://127.0.0.1:27017/Appraisal'
+    'database': 'mongodb://db/staffAppraisal'
 }
